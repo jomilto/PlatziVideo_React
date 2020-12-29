69e4f1e840effca67b49f01f2913d11e25ffd807
JLob Lap <joselobatof@gmail.com>
2020-12-28 23:19:07 -0600

    fix class to className

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import '../assets/styles/components/Search.scss';
 
 const Search = () => (
-    <section class="buscador">
-        <h2 class="buscador__title">¿Qu&eacute; quieres ver hoy?</h2>
-        <input class="input" type="text" placeholder="Buscar..." />
+    <section className="buscador">
+        <h2 className="buscador__title">¿Qu&eacute; quieres ver hoy?</h2>
+        <input className="input" type="text" placeholder="Buscar..." />
     </section> 
 );
 
