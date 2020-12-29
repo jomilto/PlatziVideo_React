12ab2e2a4c00ecb8584285d6b08bfa0c8bff649a
JLob Lap <joselobatof@gmail.com>
2020-12-28 22:53:53 -0600

    Getting data from API

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -4,15 +4,17 @@ import '../assets/styles/components/CarouselItem.scss';
 import playIcon from '../assets/static/play-icon.png';
 import plusIcon from '../assets/static/plus-icon.png';
 
-const CarouselItem = () => (
+const CarouselItem = ( { cover, title, year, contentRating, duration }) => (
     <div class="carrousel-item">
-        <img class="carrousel-item__img" src="https://images.pexels.com/photos/1183434/pexels-photo-1183434.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="gato" />
+        <img class="carrousel-item__img" src={cover} alt={title} />
         <div class="carrousel-item__details">
             <div class="carrousel-item__details--content">
                 <img src={playIcon} alt="Play" />
                 <img src={plusIcon} alt="See more" />
-                <p class="carrousel_item__details--title">Título Descriptivo</p>
-                <p class="carrousel-item__details--subtitle">2019 16+ 114 minutos</p>
+                <p class="carrousel_item__details--title">{title}</p>
+                <p class="carrousel-item__details--subtitle">
+                    {`${year} ${contentRating} ${duration} minutes`}
+                </p>
             </div>
         </div>
     </div>
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -10,7 +10,11 @@ import Footer from '../components/Footer';
 import '../assets/styles/App.scss';
 
 const App = () => {
-    const [ videos, setVideos ] = useState([]);
+    const [ videos, setVideos ] = useState({
+        mylist: [],
+        trends: [],
+        originals: []
+    });
 
     useEffect( () => {
         fetch('http://localhost:3000/initalState')
@@ -18,41 +22,39 @@ const App = () => {
             .then(data => setVideos(data));
     },[]);
 
-    console.log(videos);
-
     return (
         <div className="App">
             <Header />
             <Search />
-            <Categories title="Mi lista">
-                <Carousel>
-                    <CarouselItem/>
-                    <CarouselItem/>
-                    <CarouselItem/>
-                    <CarouselItem/>
-                    <CarouselItem/>
-                    <CarouselItem/>
-                    <CarouselItem/>
-                    <CarouselItem/>
-                    <CarouselItem/>
-                </Carousel>
-            </Categories>
+            {videos.mylist.length > 0 &&
+                <Categories title="Mi lista">
+                    <Carousel>
+                        {videos.mylist.map(item => 
+                            <CarouselItem key={item.id} {...item}/>
+                        )}
+                    </Carousel>
+                </Categories>
+            }
 
-            <Categories title="Tendencias">
-                <Carousel>
-                    <CarouselItem/>
-                    <CarouselItem/>
-                    <CarouselItem/>
-                    <CarouselItem/>
-                </Carousel>
-            </Categories>
+            {videos.trends.length > 0 &&
+                <Categories title="Tendencias">
+                    <Carousel>
+                        {videos.trends.map(item => 
+                            <CarouselItem key={item.id} {...item}/>
+                        )}
+                    </Carousel>
+                </Categories>
+            }
 
-            <Categories title="Originales">
-                <Carousel>
-                    <CarouselItem/>
-                    <CarouselItem/>
-                </Carousel>
-            </Categories>
+            {videos.originals.length > 0 &&
+                <Categories title="Originales">
+                    <Carousel>
+                        {videos.originals.map(item => 
+                            <CarouselItem key={item.id} {...item}/>
+                        )}                    
+                    </Carousel>
+                </Categories>
+            }
             <Footer />
         </div>
     );
