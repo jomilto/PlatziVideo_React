988008fa712bac573eb6272d9445e29ad8928b01
JLob Lap <joselobatof@gmail.com>
2020-12-28 23:18:45 -0600

    PropTypes

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,23 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import '../assets/styles/components/CarouselItem.scss';
 
 import playIcon from '../assets/static/play-icon.png';
 import plusIcon from '../assets/static/plus-icon.png';
 
 const CarouselItem = ( { cover, title, year, contentRating, duration }) => (
-    <div class="carrousel-item">
-        <img class="carrousel-item__img" src={cover} alt={title} />
-        <div class="carrousel-item__details">
-            <div class="carrousel-item__details--content">
+    <div className="carrousel-item">
+        <img className="carrousel-item__img" src={cover} alt={title} />
+        <div className="carrousel-item__details">
+            <div className="carrousel-item__details--content">
                 <img src={playIcon} alt="Play" />
                 <img src={plusIcon} alt="See more" />
-                <p class="carrousel_item__details--title">{title}</p>
-                <p class="carrousel-item__details--subtitle">
+                <p className="carrousel_item__details--title">{title}</p>
+                <p className="carrousel-item__details--subtitle">
                     {`${year} ${contentRating} ${duration} minutes`}
                 </p>
             </div>
         </div>
     </div>
 );
+
+CarouselItem.propTypes = {
+    cover: PropTypes.string,
+    title: PropTypes.string,
+    year: PropTypes.number,
+    contentRating: PropTypes.string,
+    duration: PropTypes.number
+}
 
 export default CarouselItem;
