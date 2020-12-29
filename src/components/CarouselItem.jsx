4435d41e873f6f64e3daa278fb034e300fbcae7c
JLob Lap <joselobatof@gmail.com>
2020-12-28 21:13:45 -0600

    Add images to project

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import '../assets/styles/components/CarouselItem.scss';
 
-const CarouselItem = ({ children }) => (
+import playIcon from '../assets/static/play-icon.png';
+import plusIcon from '../assets/static/plus-icon.png';
+
+const CarouselItem = () => (
     <div class="carrousel-item">
         <img class="carrousel-item__img" src="https://images.pexels.com/photos/1183434/pexels-photo-1183434.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="gato" />
         <div class="carrousel-item__details">
             <div class="carrousel-item__details--content">
-                <img src="./assets/img/play-icon.png" alt="Play" />
-                <img src="./assets/img/plus-icon.png" alt="See more" />
+                <img src={playIcon} alt="Play" />
+                <img src={plusIcon} alt="See more" />
                 <p class="carrousel_item__details--title">Título Descriptivo</p>
                 <p class="carrousel-item__details--subtitle">2019 16+ 114 minutos</p>
             </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import '../assets/styles/components/Header.scss';
 
+import logo from '../assets/static/logo.png';
+import userIcon from '../assets/static/user.png';
+
 const Header = () => (
     <header className="header">
-        <img className="header__img" src="./assets/img/logo.png" alt="logo_platzi_video" />
+        <img className="header__img" src={logo} alt="logo_platzi_video" />
         <div className="header__menu">
             <div className="header__menu--profile">
-                <img src="./assets/img/user.png" alt="User" />
+                <img src={userIcon} alt="User" />
                 <p>Perfil</p>
             </div>
             <ul>
