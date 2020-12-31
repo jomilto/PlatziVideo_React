afade331756a3132f95955065f3dcb004e56281d
JLob Lap <joselobatof@gmail.com>
2020-12-30 20:59:05 -0600

    use of gravatar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,52 @@
 import React from 'react';
+import { connect } from "react-redux";
 import { Link } from 'react-router-dom';
 
 import '../assets/styles/components/Header.scss';
 import logo from '../assets/static/logo.png';
-import userIcon from '../assets/static/user.png';
+// import userIcon from '../assets/static/user.png';
+import gravatar from "../utils/gravatar";
 
-const Header = () => (
-    <header className="header">
-        
-        <Link to="/">
-            <img className="header__img" src={logo} alt="logo_platzi_video" />
-        </Link>
+const Header = (props) => {
+    const { user } = props;
 
-        <div className="header__menu">
-            <div className="header__menu--profile">
-                <img src={userIcon} alt="User" />
-                <p>Perfil</p>
-            </div>
-            <ul>
-                <li>
-                    <a href="#">Cuenta</a>
-                </li>
-                <li>
-                    <a href="#">Cerrar Sesión</a>
-                </li>
-            </ul>
-        </div>
-    </header>
-);
+    const hasUser = Object.keys(user).length > 0;
 
-export default Header;
+    return (
+        <header className="header">
+            
+            <Link to="/">
+                <img className="header__img" src={logo} alt="logo_platzi_video" />
+            </Link>
+
+            {hasUser &&
+                <div className="header__menu">
+                    <div className="header__menu--profile">
+                        <img 
+                            src={ gravatar(user.email) } 
+                            alt="User" 
+                        />
+                        <p>Perfil</p>
+                    </div>
+                    <ul>
+                        <li>
+                            <a href="#">Cuenta</a>
+                        </li>
+                        <li>
+                            <a href="#">Cerrar Sesión</a>
+                        </li>
+                    </ul>
+                </div>
+            }
+        </header>
+    )
+};
+
+const mapStateToProps = (state) => {
+    console.log(state);
+    return {
+        user: state.user
+    };
+};
+
+export default connect(mapStateToProps,null)(Header);
