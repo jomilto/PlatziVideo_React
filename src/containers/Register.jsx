81f4ce34f5494d94fdb66b2dbb75a0d56ef64660
JLob Lap <joselobatof@gmail.com>
2020-12-30 21:52:09 -0600

    add login to register

--- src/containers/Register.jsx
+++ src/containers/Register.jsx
@@ -1,24 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import '../assets/styles/components/Register.scss'
 
-const Register = () => (
-    <section className="registro">
-        <div className="registro__container">
-            <h2>Registrate</h2>
-            <form action="" className="registro__container--form">
-                <input className="input" type="text" name="" id="" placeholder="Nombre" />
-                <input className="input" type="text" name="" id="" placeholder="Apellidos" />
-                <input className="input" type="text" name="" id="" placeholder="Correo" />
-                <input className="input" type="password" name="" id="" placeholder="Contraseña" />
-                <button className="button">Crear Cuenta</button>
-            </form>
-            <Link to="/login">
-                Iniciar Sesión
-            </Link>
-        </div>
-    </section>
-);
+const Register = () => {
+    const [ form, setValues ] = useState({
+        email: '',
+        name: '',
+        password: ''
+    })
+
+    const handleInput = (event) => {
+        setValues({
+            ...form,
+            [event.target.name]: event.target.value
+        });
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(form);
+    };
+
+    return (
+        <section className="registro">
+            <div className="registro__container">
+                <h2>Registrate</h2>
+                <form onSubmit={handleSubmit} className="registro__container--form">
+                    <input 
+                        className="input" 
+                        type="text" 
+                        name="name" 
+                        id="name" 
+                        placeholder="Nombre"
+                        onChange={handleInput}
+                    />
+                    <input 
+                        className="input" 
+                        type="email" 
+                        name="email" 
+                        id="email" 
+                        placeholder="Correo"
+                        onChange={handleInput} 
+                    />
+                    <input 
+                        className="input" 
+                        type="password" 
+                        name="password" 
+                        id="password" 
+                        placeholder="Contraseña"
+                        onChange={handleInput}
+                    />
+                    <button className="button">Crear Cuenta</button>
+                </form>
+                <Link to="/login">
+                    Iniciar Sesión
+                </Link>
+            </div>
+        </section>
+    )
+};
 
 export default Register;
