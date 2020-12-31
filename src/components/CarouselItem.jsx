d7d080f850a41fb44327cf478a900f47ae240473
JLob Lap <joselobatof@gmail.com>
2020-12-30 19:59:36 -0600

    Delete item from favorite

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { setFavorite } from '../actions';
+import { setFavorite, deleteFavorite } from '../actions';
 import PropTypes from 'prop-types';
 import '../assets/styles/components/CarouselItem.scss';
 
 import playIcon from '../assets/static/play-icon.png';
 import plusIcon from '../assets/static/plus-icon.png';
+import removeIcon from '../assets/static/remove-icon.png';
 
 const CarouselItem = (props) => {
     const { id, cover, title, year, contentRating, duration } = props;
@@ -16,6 +17,10 @@ const CarouselItem = (props) => {
         });
     }
 
+    const handleDeleteFavorite = (itemId) => {
+        props.deleteFavorite(itemId);
+    }
+
     return (
         <div className="carrousel-item">
             <img className="carrousel-item__img" src={cover} alt={title} />
@@ -23,6 +28,7 @@ const CarouselItem = (props) => {
                 <div className="carrousel-item__details--content">
                     <img src={playIcon} alt="Play" />
                     <img src={plusIcon} alt="Add to Favorites" onClick={handleSetFavorite}/>
+                    <img src={removeIcon} alt="Delete from Favorites" onClick={() => handleDeleteFavorite(id)}/>
                     <p className="carrousel_item__details--title">{title}</p>
                     <p className="carrousel-item__details--subtitle">
                         {`${year} ${contentRating} ${duration} minutes`}
@@ -44,6 +50,7 @@ CarouselItem.propTypes = {
 
 const mapDispatchToProps = {
     setFavorite,
+    deleteFavorite
 };
 
 export default connect(null, mapDispatchToProps)(CarouselItem);
